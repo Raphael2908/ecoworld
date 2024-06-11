1739876be0380b7e41aef7a235059a86307186cc
ElliottWong <82916695+ElliottWong@users.noreply.github.com>
2024-06-11 13:51:16 +0000

    feat: rollsCounter

--- hooks/useReward.tsx
+++ hooks/useReward.tsx
@@ -1,5 +1,5 @@
+import { supabase } from '@/helpers/supabase';
 import { useEffect, useState } from 'react';
-
 function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
@@ -9,12 +9,24 @@ function useReward(initialTaps = 0) {
     const [taps, setTaps] = useState(initialTaps);
     const [rolls, setRolls] = useState(0);
     const [reward, setReward] = useState<string | null>(null);
-    
+
+    async function getRolls() {
+        let { data: { session } } = await supabase.auth.getSession()
+        const test = await supabase.from('rolls').select('*').eq('user_id', session?.user.id);
+        if (test.data?.length) {
+            console.log(test.data[0].rolls, "test")
+            setRolls(test.data[0].rolls)
+        }
+        else return 0
+    }
+
+    async function updateAsync() {
+        let { data: { session } } = await supabase.auth.getSession()
+        const { error: updateError } = await supabase.from('rolls').update({ 'rolls': rolls - 1 }).eq('user_id', session?.user.id)
+    }
 
     useEffect(() => {
-        // Get amount of rolls from server
-        const rollsFromServer = 1;
-        setRolls(rollsFromServer);
+        getRolls()
     }, []);
 
     const increment = () => {
@@ -37,7 +49,8 @@ function useReward(initialTaps = 0) {
 
             // Reset count
             setTaps(0);
-            setRolls(0);
+            setRolls(rolls - 1);
+            updateAsync()
         }
     }, [taps, rolls]);
 
