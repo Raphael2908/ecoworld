c3fde31532671a9308b8c072546759719fb92c07
proprogramerx <jerometeoh@gmail.com>
2024-06-12 23:40:51 +0800

    removed storage of images & added vercel

--- app/camera.jsx
+++ app/camera.jsx
@@ -16,7 +16,7 @@ export default function camera() {
 
   useEffect(() => {
     (async () => {
-      MediaLibrary.requestPermissionsAsync();
+      //MediaLibrary.requestPermissionsAsync();
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
       setHasCameraPermission(cameraStatus.status === 'granted');
     })();
@@ -37,11 +37,11 @@ export default function camera() {
   const savePicture = async () => {
     if (image) {
       try {
-        const asset = await MediaLibrary.createAssetAsync(image);
-        alert('Picture saved! 🎉');
+        //const asset = await MediaLibrary.createAssetAsync(image);
+        alert('Picture Uploaded! 🎉');
         setImage(null);
         uploadImage();
-        console.log('Uploaded & Saved successfully');
+        console.log('Uploaded successfully');
       } catch (error) {
         console.log(error);
       }
@@ -150,100 +150,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-// import { CameraView, useCameraPermissions } from 'expo-camera';
-// import { useState, useRef } from 'react';
-// import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import * as MediaLibrary from 'expo-media-library';
-
-// export default function Camera() {
-//   const [facing, setFacing] = useState('back');
-//   const [permission, requestPermission] = useCameraPermissions();
-//   const cameraRef = useRef(null);
-
-//   if (!permission) {
-//     // Camera permissions are still loading.
-//     return <View />;
-//   }
-
-//   if (!permission.granted) {
-//     // Camera permissions are not granted yet.
-//     return (
-//       <View style={styles.container}>
-//         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-//         <Button onPress={requestPermission} title="grant permission" />
-//       </View>
-//     );
-//   }
-
-//   function toggleCameraFacing() {
-//     setFacing(current => (current === 'back' ? 'front' : 'back'));
-//   }
-
-//   const takePicture = async () => {
-//     if (cameraRef) {
-//       try {
-//         const data = await cameraRef.current.takePictureAsync();
-//         console.log(data);
-//         setImage(data.uri);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//   };
-
-//   const savePicture = async () => {
-//     if (image) {
-//       try {
-//         const { uri } = await Camera.takePictureAsync();
-//         const asset = await MediaLibrary.createAssetAsync(image);
-//         alert('Picture saved! 🎉');
-//         setImage(null);
-//         console.log('saved successfully');
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <CameraView style={styles.camera} facing={facing}>
-//         <View style={styles.buttonContainer}>
-//           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-//             <Text style={styles.text}>Flip Camera</Text>
-//           </TouchableOpacity>
-//           <TouchableOpacity style={styles.button} onPress={takePicture}>
-//             <Text style={styles.text}>Take Picture</Text>
-//           </TouchableOpacity>
-//           <Button title="Save" onPress={savePicture} icon="check" />
-//         </View>
-//       </CameraView>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-//   camera: {
-//     flex: 1,
-//   },
-//   buttonContainer: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     backgroundColor: 'transparent',
-//     margin: 64,
-//   },
-//   button: {
-//     flex: 1,
-//     alignSelf: 'flex-end',
-//     alignItems: 'center',
-//   },
-//   text: {
-//     fontSize: 24,
-//     fontWeight: 'bold',
-//     color: 'white',
-//   },
-// });
