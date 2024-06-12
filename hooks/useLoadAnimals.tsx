1035beba913cdbbb483c9a4cbfb102acc233561d
Raphael Lim <raphaellim@Raphaels-Mac-mini.local>
2024-06-12 21:49:32 +0800

    feat: proper display of animals

--- hooks/useLoadAnimals.tsx
+++ hooks/useLoadAnimals.tsx
@@ -1,17 +1,36 @@
+import { supabase } from '@/helpers/supabase';
 import { useEffect, useState } from 'react';
 
 interface Animal { 
     name: string,
-    user_id: string
+    user_id: string, 
+    created_at: string, 
+    id: string
 }
 // Custom hook to manage reward / gacha logic
-function useLoadAnimals() {
+async function useLoadAnimals() {
     // send request to get animals 
-    const [animals, setAnimals] = useState<Array<Animal>>([{name: "wayne", user_id:'Raphaeluuid'}])
-    useEffect(() => {
-        // Data fetch animals from database
-    })
-    return { animals };
+    const animals : Array<Animal> = []
+    // Get the initial session
+    let { data: { session } } = await supabase.auth.getSession();
+
+    // Set up a listener for auth state changes
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, newSession) => {
+        session = newSession;
+    });
+
+    // Ensure that the session is not null
+    if (session == null) {
+        throw new Error("please sign in");
+    }
+
+    // Fetch the current rolls value for the user
+    const { data: currentData, error: fetchError } = await supabase
+        .from('animals')
+        .select('*')
+        .eq('user_id', session.user.id)
+
+    return currentData;
 }
 
 export default useLoadAnimals;
