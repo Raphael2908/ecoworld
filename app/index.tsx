4d944466a6f36b0b79c01afffefa8c61d4da18f0
Raphael Lim <raphaellim@Raphaels-Mac-mini.local>
2024-06-13 03:11:39 +0800

    fix: backend url

--- app/index.tsx
+++ app/index.tsx
@@ -19,7 +19,7 @@ const App = () => {
       setSession(session)
     })
   }, [])
-
+  
   if(!session){
     return (
       <Auth/>
--- helpers/getAnimalSpeech.ts
+++ helpers/getAnimalSpeech.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const getAnimalSpeech = async (animal: string) => {
     try {
       const result: any = await axios.get(
-        `https://ecoworld-backend-4x2dgd7dz-raphaels-projects-54b089a5.vercel.app/getAnimalSpeech?animal=${animal}`,
+        `https://ecoworld-backend.vercel.app/getAnimalSpeech?animal=${animal}`,
       )
       return result.data
     } catch (error) {
